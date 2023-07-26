e118a470c2bba84ebcfce53a2a97266f83bfef48
Hassaan-Qaisar <106833785+Hassaan-Qaisar@users.noreply.github.com>
2023-07-26 13:10:17 +0500

    Update TodoForm.js

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,21 +1,34 @@
 import React, { useState } from "react";
+
  
 const TodoForm = (props) => {
   const [value, setValue] = useState("");
+  const [isError, setIsError] = useState(false);
 
   const onChangeHandler = (event) => {
+    // if (!event.target.value) {
+    //   setIsError(true);
+    // }
+
     setValue(event.target.value)
   }
 
   const onSubmitHandler = (event) => {
     event.preventDefault();
 
+    if (value.trim().length === 0) {
+      setIsError(true)
+      return;
+    }
     props.onAdd(value)
 
     setValue("")
+    setIsError(false)
   }
 
   return (
+    <React.Fragment>
+
     <form className="TodoForm" onSubmit={onSubmitHandler} >
       <input
         type="text"
@@ -27,7 +40,9 @@ const TodoForm = (props) => {
       <button type="submit" className="todo-btn">
         Add Task
       </button>
+      {isError && <div className="display-error"> <p>Empty Field! Enter a valid task</p></div>}
     </form>
+    </React.Fragment>
   );
 };
 
