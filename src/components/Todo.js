ba525a0421e301bf0851a87ff8ada1c9b2f2c663
Hassaan-Qaisar <106833785+Hassaan-Qaisar@users.noreply.github.com>
2023-07-26 13:09:16 +0500

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,41 +1,46 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+// import { faTrash } from "@fortawesome/free-solid-svg-icons";
+
+import { FaTrash } from "react-icons/fa";
+import { FaEdit } from "react-icons/fa";
 
 const Todo = (props) => {
-    const onClickParaHandler = () => {
-        props.onToggleComplete(props.id);
-        
-    }
+  const onClickParaHandler = () => {
+    props.onToggleComplete(props.id);
+  };
 
-    const onDeleteHandler = () => {
-        props.onDeleteTodo(props.id);
-    }
+  const onDeleteHandler = () => {
+    props.onDeleteTodo(props.id);
+  };
 
-    const onEditHandler = () => {
-        props.onEditTodo(props.id);
-    }
+  const onEditHandler = () => {
+    props.onEditTodo(props.id);
+  };
 
   return (
     <div className="Todo">
       <p
-        className={`${props.completed ? 'completed' : ''}`}
+        className={`${props.completed ? "completed" : ""}`}
         onClick={onClickParaHandler}
-        
       >
         {props.task}
       </p>
       <div>
-        <FontAwesomeIcon
-          icon={faPenToSquare}
-          onClick={onEditHandler}
-          
-        />
-        <FontAwesomeIcon icon={faTrash} onClick={onDeleteHandler} />
+        <span className="iconspan">
+        <FaEdit onClick={onDeleteHandler} />
+        </span>
+        <span>
+        <FaTrash onClick={onEditHandler} />
+        </span>
+
+        {/* <FontAwesomeIcon icon={faPenToSquare} onClick={onEditHandler} />
+        <FontAwesomeIcon icon={faTrash} onClick={onDeleteHandler} /> */}
       </div>
     </div>
   );
 };
 
 export default Todo;
+
